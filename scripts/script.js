49b56e988cbc492f7a15e275acfe100252d727a0
noabos <74159609+noabos@users.noreply.github.com>
2022-04-28 16:54:36 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//SKYSCRAPER 1
 document.getElementById('btn1').addEventListener('click', function () {
     let spidey1 = document.querySelector('.spidey1');
     spidey1.classList.add('slinger');
@@ -8,6 +9,23 @@ document.getElementById('btn1').addEventListener('click', function () {
 });
 
 
+var textbox = document.querySelector("#textbox1");
+var button = document.querySelector("#btn1");
+
+button.addEventListener ("click", message1);
+
+function message1 ()
+{
+    document.getElementById("textbox1").innerHTML = "This white and blue suit gave Spiderman enhanced strength, speed and telekenises";
+};
+
+setTimeout(() => {
+  const box = document.getElementById('textbox1');
+  box.style.display = 'none';
+}, 7000);
+
+
+//SKYSCRAPER 2
 document.getElementById('btn2').addEventListener('click', function () {
     let spidey1 = document.querySelector('.spidey2');
     spidey1.classList.add('slinger');
@@ -17,6 +35,24 @@ document.getElementById('btn2').addEventListener('click', function () {
     });
 });
 
+var textbox = document.querySelector("#textbox2");
+var button = document.querySelector("#btn2");
+
+button.addEventListener ("click", message2);
+
+function message2 ()
+{
+    document.getElementById("textbox2").innerHTML = "This grey and black amored suit protest Spiderman from weapons.";
+};
+
+
+setTimeout(() => {
+  const box = document.getElementById('textbox2');
+  box.style.display = 'none';
+}, 7000);
+
+
+//SKYSCRAPER 3
 document.getElementById('btn3').addEventListener('click', function () {
     let spidey1 = document.querySelector('.spidey3');
     spidey1.classList.add('slinger');
@@ -26,6 +62,24 @@ document.getElementById('btn3').addEventListener('click', function () {
     });
 });
 
+var textbox = document.querySelector("#textbox3");
+var button = document.querySelector("#btn3");
+
+button.addEventListener ("click", message3);
+
+function message3 ()
+{
+    document.getElementById("textbox3").innerHTML = "With this suit, Spiderman gives out presents.";
+};
+
+
+setTimeout(() => {
+  const box = document.getElementById('textbox3');
+  box.style.display = 'none';
+}, 7000);
+
+
+//SKYSCRAPER 4
 document.getElementById('btn4').addEventListener('click', function () {
     let spidey1 = document.querySelector('.spidey4');
     spidey1.classList.add('slinger');
@@ -35,7 +89,24 @@ document.getElementById('btn4').addEventListener('click', function () {
     });
 });
 
+var textbox = document.querySelector("#textbox4");
+var button = document.querySelector("#btn4");
 
+button.addEventListener ("click", message4);
+
+function message4 ()
+{
+    document.getElementById("textbox4").innerHTML = "Spiderman used this suit for the Secret War.";
+};
+
+
+setTimeout(() => {
+  const box = document.getElementById('textbox4');
+  box.style.display = 'none';
+}, 7000);
+
+
+//SKYSCRAPER 5
 document.getElementById('btn5').addEventListener('click', function () {
     let spidey5 = document.querySelector('.spidey5');
     spidey5.classList.add('dangle');
@@ -45,7 +116,23 @@ document.getElementById('btn5').addEventListener('click', function () {
     }, 11000);
 });
 
+var textbox = document.querySelector("#textbox5");
+var button = document.querySelector("#btn5");
 
+button.addEventListener ("click", message5);
+
+function message5 ()
+{
+    document.getElementById("textbox5").innerHTML = "This suit was a gift from Tony Stark, completed with mechanical spider-arms.";
+};
+
+setTimeout(() => {
+  const box = document.getElementById('textbox5');
+  box.style.display = 'none';
+}, 11000);
+
+
+//SKYSCRAPER 6
 document.getElementById('btn6').addEventListener('click', function () {
     let spidey1 = document.querySelector('.spidey6');
     spidey1.classList.add('slinger');
@@ -55,6 +142,24 @@ document.getElementById('btn6').addEventListener('click', function () {
     });
 });
 
+
+var textbox = document.querySelector("#textbox6");
+var button = document.querySelector("#btn6");
+
+button.addEventListener ("click", message6);
+
+function message6 ()
+{
+    document.getElementById("textbox6").innerHTML = "Spiderman used this suit to join the Future Foundation.";
+};
+
+setTimeout(() => {
+  const box = document.getElementById('textbox6');
+  box.style.display = 'none';
+}, 7000);
+
+
+//SKYSCRAPER 7
 document.getElementById('btn7').addEventListener('click', function () {
     let spidey1 = document.querySelector('.spidey7');
     spidey1.classList.add('slinger');
@@ -64,9 +169,47 @@ document.getElementById('btn7').addEventListener('click', function () {
     });
 });
 
+var textbox = document.querySelector("#textbox7");
+var button = document.querySelector("#btn7");
 
+button.addEventListener ("click", message7);
+
+function message7 ()
+{
+    document.getElementById("textbox7").innerHTML = "This black and green suits, makes Spiderman invisible.";
+};
+
+setTimeout(() => {
+  const textbox7 = document.getElementById('textbox7');
+  textbox7.style.display = 'none';
+}, 7000);
+
+
+//SKYSCRAPER 8
 document.getElementById('btn8').addEventListener('click', function () {
-  document.querySelector('.spidey8').classList.add('lastone');
+    document.querySelector('.spidey8').classList.add('lastone');
 });
 
+var textbox = document.querySelector("#textbox8");
+var button = document.querySelector("#btn8");
+
+button.addEventListener ("click", message8);
+
+function message8 ()
+{
+    document.getElementById("textbox8").innerHTML = "The newest suit of Spiderman, with darker richer colours than te previous suit.";
+};
+
+
+setTimeout(() => {
+  const textbox8 = document.getElementById('textbox8');
+  textbox8.style.display = 'none';
+}, 7000);
+
+
 //bron: https://forum.freecodecamp.org/t/trigger-animations-with-button-press/214733/4 en Sanne!
+
+
+
+
+
